d1e721b07bb4c2ca8df5bb8879d2e6c8b815a90b
Игнатье Сергей <sergey@localhost.localdomain>
2018-03-22 18:30:01 +0700

    task2 ready

--- menu.js
+++ menu.js
@@ -62,6 +62,9 @@ MenuItem.prototype.renderItem = function () {
     return '<li><a href="' + this.href + '">' + this.title + '</a></li>';
 };
 
+/**
+ */
+
 
 // глобальные данные
 let menuMain;
@@ -80,6 +83,12 @@ function createMenu() {
                 new MenuItem('/about', 'О нас'),
                 new MenuItem('/service', 'Услуги'),
                 new MenuItem('/blog', 'Блог'),
+                new Menu('my2', 'my2', [
+                    new MenuItem('/blog/php', 'php'),
+                    new MenuItem('/blog/js', 'js'),
+                    new MenuItem('/blog/mysql', 'mysql'),
+                    new MenuItem('/blog/yii2', 'yii2'),
+                ]),
                 new MenuItem('/contacts', 'Контакты'),
                 //new Menu('34', '324', [])
             ]);
@@ -93,11 +102,50 @@ function deleteMenu() {
 }
 
 
+/**
+ * Создаем класс SubMenu со вложенными меню, родитель класс Menu
+ * @param {string} myId         Id меню
+ * @param {string} myClass      Класс меню  - используется для стилизации
+ * @param {array} [myItems = []]  Пункты меню, по умолчанию их нет
+ */
+function SubMenu(myId, myClass, myItems = []) {
+    this.id = myId;
+    this.className = myClass;
+    this.items = myItems;
+}
+
+// Наследуем все методы класса Menu
+SubMenu.prototype.__proto__ = Menu.prototype;
+
+/**
+ * Метод класса SubMenu
+ * @param   {array} [myItems = []] массив пунктов меню: заголовок и ссылка
+ * @returns {string} Возвращает построеное меню в html коде
+ */
+SubMenu.prototype.render = function (myItems = []) {
+
+    if (myItems.length !== 0)
+        this.items = myItems;
+
+    var result5 = '<ul class="' + this.className + '" id="' + this.id + '">';
+
+    for (var i = 0; i < this.items.length; i++) {
+
+        if (this.items[i] instanceof Menu) {
+            result5 += this.items[i].render();
+        } else if (this.items[i] instanceof MenuItem) {
+            result5 += this.items[i].renderItem(); //result5 = result5 + this.items[i].renderItem();
+        }
+    }
+
+    result5 += '</ul>';
+    return result5;
+};
 
 window.onload = function () {
 
     menu1 = document.getElementById('menu1');
-    menuMain = new Menu('my1', 'my1');
+    menuMain = new SubMenu('my1', 'my1');
 
     createMenuButton = document.getElementById("createMenu");
     deleteMenuButton = document.getElementById("deleteMenu");
